9b548248229d787c16e9395e0031b8949696930a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-12 16:05:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,5 +10,19 @@ module FormsLab
     get '/new' do
       erb :'pirates/new'
     end
+
+    post '/pirates' do
+      @pirate = Pirate.new params[:pirate]
+
+      params[:pirate][:ships].each do |ship|
+        Ship.new ship
+      end
+
+      @ship1 = Ship.all.first
+      @ship2 = Ship.all[1]
+
+      erb :'pirates/show'
+    end
+
   end
 end

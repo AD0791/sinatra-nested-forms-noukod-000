cc590da6436fdd212f437e19db67e72844f93ca7
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-12 16:02:09 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  Pirates = []
+  def initialize(name,weight,height)
+    @name= name
+    @weight = weight
+    @height = height
+    Pirates << self
+  end
+
+  def self.all
+    Pirates
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_reader :name, :type, :booty
+  Ship = []
+
+  def initialize(name,type,booty)
+    @name = name
+    @type = type
+    @booty = booty
+    Ship << self
+  end
+
+  def self.all
+    Ship
+  end
+
+  def self.clear
+    Ship.clear
+  end
 end
